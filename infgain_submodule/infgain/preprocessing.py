f7a615d962bf0d3d3f65bf345a4c7b8a4549cae2
Anton Karazeev <anton.karazeev@gmail.com>
2017-10-31 18:39:32 +0300

    Add more methods to infgain.preprocessing

--- infgain_submodule/infgain/preprocessing.py
+++ infgain_submodule/infgain/preprocessing.py
@@ -1,6 +1,8 @@
 import typing
 import nltk
 import re
+import itertools
+import collections
 
 
 NUMBER_TOKEN = 'NUMBER'
@@ -77,7 +79,7 @@ def word_tokenize(string: str) -> list:
     Tokenize `string` into words and replace numbers with `NUMBER_TOKEN`.
     :param string:
     :return:
-    >>> tokenized = cool_tokenize('Mean age was 74.5+/-9.0 years (men: 63.2%) and mean CHADS2 score (+/-SD) was 1.8+/-1.2.')
+    >>> tokenized = word_tokenize('Mean age was 74.5+/-9.0 years (men: 63.2%) and mean CHADS2 score (+/-SD) was 1.8+/-1.2.')
     >>> tokenized == ['mean','age','was','NUMBER','years','men','NUMBER','and','mean','chads2','score','sd','was','NUMBER']
     True
     """
@@ -127,6 +129,185 @@ def remove_bad_ngrams(ngrams: list) -> list:
     True
     """
     return list(filter(lambda x: contains_stopword(x) == False, ngrams))
+
+
+def get_unigrams(ngrams: list) -> list:
+    """
+    :param ngrams: tuples of ngrams
+    :return: list of str
+    >>> ngrams = [('background',),('rivaroxaban',),('currently',),('used',),('background', 'rivaroxaban'),('currently', 'used')]
+    >>> get_unigrams(ngrams) == ['background', 'rivaroxaban', 'currently', 'used']
+    True
+    """
+    unigram_tuples = list(filter(lambda x: len(x) == 1, ngrams))
+    unigrams = list(map(lambda x: x[0], unigram_tuples))
+    return unigrams
+
+
+def make_sentence(ngrams: list) -> str:
+    """
+    :param ngrams:
+    :return:
+    >>> ngrams = [('background',),('rivaroxaban',),('currently',),('used',),('background', 'rivaroxaban')]
+    >>> make_sentence(ngrams) == 'background rivaroxaban currently used'
+    True
+    """
+    unigrams = get_unigrams(ngrams)
+    return ' '.join(unigrams)
+
+
+def ngrams_from_dict(dictionary: dict) -> list:
+    return list(filter(lambda x: NGRAM_SEP in x, dictionary.keys()))
+
+
+def tildas_from_dict(dictionary: dict) -> list:
+    return list(filter(lambda x: TILDA_SEP in x, dictionary.keys()))
+
+
+def make_dict(ngrams: list, threshold=3) -> tuple:
+    """
+    :param ngrams: list of lists of ngram tuples
+    :param threshold: min frequency of token
+    :return: `dictionary` {token: id, ...} and `reversed_dictionary` {id: token, ...}
+    """
+    def ngrams_to_str(ngrams: list) -> list:
+        """
+        >>> ngrams = [('used',),('background', 'rivaroxaban')]
+        >>> ngrams_to_str(ngrams) == ['used', 'background_rivaroxaban']
+        True
+        """
+        return list(map(lambda x: NGRAM_SEP.join(x), ngrams))
+
+    def merge_list(ngrams_list: list) -> list:
+        """
+        >>> merge_list([[1,2,3], [4]]) == [1, 2, 3, 4]
+        True
+        """
+        return list(itertools.chain.from_iterable(ngrams_list))
+
+    cntr = collections.Counter()
+    cntr.update(ngrams_to_str(merge_list(ngrams)))
+    cntr = list(filter(lambda x: x[1] > threshold, cntr.items()))
+
+    dictionary = {UNK_TOKEN: 0}
+    for item, _ in cntr:
+        dictionary[item] = len(dictionary)
+
+    def make_tilda_tokens(dictionary: dict) -> list:
+        """
+        Convert ngrams from `dictionary` into tilda tokens.
+        :param dictionary:
+        :return:
+        """
+        def to_tilda_tokens(ngrams: list) -> list:
+            """
+            >>> ngrams = [['atrial', 'fibrillation'], ['help', 'may']]
+            >>> to_tilda_tokens(ngrams) == ['atrial~fibrillation', 'help~may']
+            True
+            """
+            return list(map(lambda x: TILDA_SEP.join(x), ngrams))
+
+        def sort_ngrams(ngrams: list) -> list:
+            """
+            >>> ngrams = [['atrial', 'fibrillation'], ['may', 'help']]
+            >>> sort_ngrams(ngrams) == [['atrial', 'fibrillation'], ['help', 'may']]
+            True
+            """
+            return list(map(lambda x: sorted(x), ngrams))
+
+        def split_ngrams(ngrams: list) -> list:
+            """
+            >>> ngrams = ['currently_used', 'atrial_fibrillation', 'may_help']
+            >>> split_ngrams(ngrams) == [['currently', 'used'], ['atrial', 'fibrillation'], ['may', 'help']]
+            True
+            """
+            return list(map(lambda x: x.split(NGRAM_SEP), ngrams))
+
+        return list(set(to_tilda_tokens(sort_ngrams(split_ngrams(ngrams_from_dict(dictionary))))))
+
+    tilda_tokens = make_tilda_tokens(dictionary)
+
+    for item in tilda_tokens:
+        dictionary[item] = len(dictionary)
+
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+
+    return dictionary, reversed_dictionary
+
+
+def replace_unk(string: str, dictionary: dict) -> str:
+    """
+    Replace out-of-vocabulary words with `UNK_TOKEN`.
+    :param string:
+    :param dictionary:
+    :return:
+    >>> replace_unk('to amino') == 'UNK amino'
+    True
+    """
+    list_unk = list(map(lambda x: x if x in dictionary else UNK_TOKEN, string.split()))
+    return ' '.join(list_unk)
+
+
+def replace_unk_list(words: list, dictionary: dict) -> str:
+    """
+    Replace out-of-vocabulary words with `UNK_TOKEN`.
+    :param words:
+    :param dictionary:
+    :return:
+    >>> replace_unk_list(['to', 'amino']) == 'UNK amino'
+    True
+    """
+    list_unk = list(map(lambda x: x if x in dictionary else UNK_TOKEN, words))
+    return ' '.join(list_unk)
+
+
+def indices_for_ngram(ngram: str, dictionary: dict) -> tuple:
+    indices = tuple(list(map(lambda x: dictionary[x], ngram.split(NGRAM_SEP))))
+    return tuple([tuple(indices), dictionary[ngram]])
+
+
+def indices_for_tilda(ngram: str, dictionary: dict) -> tuple:
+    indices = sorted(list(map(lambda x: dictionary[x], ngram.split(TILDA_SEP))))
+    indices = tuple(indices)
+    return tuple([tuple(sorted(indices)), dictionary[ngram]])
+
+
+def make_tupled_dicts(dictionary: dict) -> tuple:
+    """
+    :param dictionary:
+    :return: `dict_ngram_id_by_tuple`, `dict_tilda_id_by_tuple`
+    """
+    dict_ngram_id_by_tuple = {}
+    dict_tilda_id_by_tuple = {}
+
+    # ngrams
+    for ngram in ngrams_from_dict(dictionary):
+        tmp = indices_for_ngram(ngram, dictionary)
+        words_tup, ngram_id = tmp
+        dict_ngram_id_by_tuple[words_tup] = ngram_id
+
+    # tildas
+    for tilda in tildas_from_dict(dictionary):
+        tmp = indices_for_tilda(tilda, dictionary)
+        words_tup, tilda_id = tmp
+        dict_tilda_id_by_tuple[words_tup] = tilda_id
+
+    return dict_ngram_id_by_tuple, dict_tilda_id_by_tuple
+
+
+def ngram_id(ngram_tuple: tuple, dict_ngram_id_by_tuple: dict) -> int:
+    if ngram_tuple in dict_ngram_id_by_tuple:
+        return dict_ngram_id_by_tuple[ngram_tuple]
+    else:
+        return 0
+
+
+def tilda_id(tilda_tuple: tuple, dict_tilda_id_by_tuple: dict) -> int:
+    tilda_tuple = tuple(sorted(tilda_tuple))
+    if tilda_tuple in dict_tilda_id_by_tuple:
+        return dict_tilda_id_by_tuple[tilda_tuple]
+    else:
+        return 0
 
 if __name__ == "__main__":
     raise RuntimeError
