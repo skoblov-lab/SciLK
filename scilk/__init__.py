f518a8c7f30287b03e9e498553310bbbb5dfbaf4
Ilia Korvigo <ilia.korvigo@gmail.com>
2018-02-11 22:16:34 +0300

    replacing the rather limited `util.preprocessing.join` with a far more flexible `stack` function

--- scilk/__init__.py
+++ scilk/__init__.py
@@ -15,8 +15,8 @@ if sys.version_info < (3, 5, 2):
     sys.exit(1)
 
 
-SCILK_ROOT = (os.path.abspath(os.environ.get('SCILK_ROOT')) or
-              os.path.expanduser('~/.scilk'))
+SCILK_ROOT = os.path.abspath(os.environ.get('SCILK_ROOT') or
+                             os.path.expanduser('~/.scilk'))
 os.makedirs(SCILK_ROOT, exist_ok=True)
 
 
